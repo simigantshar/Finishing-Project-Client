9023f3d1e7e437c0c216a23b30b4068947d71a85
Simi Gantshar <sgantshar@gmail.com>
2023-09-26 16:31:20 +0300

    ok

--- src/components/layout/header dropdown/CollectionsDrop.jsx
+++ src/components/layout/header dropdown/CollectionsDrop.jsx
@@ -1,113 +1,215 @@
-import React from "react";
+import React, { useState } from "react";
 import "../header.css";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 const CollectionsDrop = () => {
-  const shopObj = [
-    {
-      type: "Watches",
-      f1: "Skeleton",
-      link1: "/watches/skeleton",
-      f2: "Tourbillons",
-      link2: "/watches/tourbillons",
-      f3: "Chronographs",
-      link3: "/watches/chronographs",
-      f4: "Moon-Phase Watches",
-      link4: "/watches/moon-phase",
-      f5: "Automatic Watches",
-      link5: "/watches/automatic",
-      f6: "All Watches",
-      link6: "/watches",
-    },
-    {
-      type: "Bands",
-      f1: "Leather",
-      link1: "/bands/leather",
-      f2: "Rubber",
-      link2: "/bands/rubber",
-      f3: "Metal",
-      link3: "/bands/metal",
-      f4: "Perlon",
-      link4: "/bands/perlon",
-      f5: "Suede",
-      link5: "/bands/suede",
-      f6: "All Bands",
-      link6: "/bands",
-    },
-    {
-      type: "Cufflinks",
-      f1: "Gear",
-      link1: "/cufflinks/gear",
-      f2: "Compass",
-      link2: "/cufflinks/compass",
-      f3: "Geneva",
-      link3: "/cufflinks/geneva",
-      f4: "Diver's",
-      link4: "/cufflinks/divers",
-      f5: "Watch Dial",
-      link5: "/cufflinks/watch-dial",
-      f6: "All Cufflinks",
-      link6: "/cufflinks",
-    },
-  ];
+
+  const [watches, setWatches] = useState(false)
+  const [brands, setBrands] = useState(false)
+  const [collections, setCollections] = useState(false)
+
+
+  const shop = ["Watches", "Bands", "Cufflinks"];
+
+  const watchShop = ["Brands", "Collection's"];
+
+  const watchCollections = {
+    type: "Collection's",
+    f1: "Skeleton",
+    link1: "/watches/skeleton",
+    f2: "Tourbillons",
+    link2: "/watches/tourbillons",
+    f3: "Chronographs",
+    link3: "/watches/chronographs",
+    f4: "Moon-Phase Watches",
+    link4: "/watches/moon-phase",
+    f5: "Automatic Watches",
+    link5: "/watches/automatic",
+    f6: "All Watches",
+    link6: "/watches",
+  };
+
+  const watchBrands = {
+    type: "Brands",
+    f1: "Rolex",
+    link1: "/watches/rolex",
+    f2: "Patek Philippe",
+    link2: "/watches/patek-philippe",
+    f3: "Audemars Piguet",
+    link3: "/watches/audemars-piguet",
+    f4: "H. MOSER & CIE.",
+    link4: "/watches/h-moser-&-cie",
+    f5: "A. LANGE & SÖHNE",
+    link5: "/watches/a-lange-&-söhne",
+    f6: "DE BETHUNE",
+    link6: "/watches/de-bethune",
+  };
+
+  // console.log(watchBrands.link4.toLowerCase())
+
+  // {
+  //   type: "Bands",
+  //   f1: "Leather",
+  //   link1: "/bands/leather",
+  //   f2: "Rubber",
+  //   link2: "/bands/rubber",
+  //   f3: "Metal",
+  //   link3: "/bands/metal",
+  //   f4: "Perlon",
+  //   link4: "/bands/perlon",
+  //   f5: "Suede",
+  //   link5: "/bands/suede",
+  //   f6: "All Bands",
+  //   link6: "/bands",
+  // },
+  // {
+  //   type: "Cufflinks",
+  //   f1: "Gear",
+  //   link1: "/cufflinks/gear",
+  //   f2: "Compass",
+  //   link2: "/cufflinks/compass",
+  //   f3: "Geneva",
+  //   link3: "/cufflinks/geneva",
+  //   f4: "Diver's",
+  //   link4: "/cufflinks/divers",
+  //   f5: "Watch Dial",
+  //   link5: "/cufflinks/watch-dial",
+  //   f6: "All Cufflinks",
+  //   link6: "/cufflinks",
+  // },
 
   return (
     <div className="fixed z-50 py-4 bg-white w-full h-[60%]">
       <div className="mx-20 grid grid-cols-4 h-full">
-        {shopObj.map((item, i) => (
-          <div key={i}
-            className={`${
-              i !== 2 && "border-r"
-            } grid grid-flow-row border-black`}
+        <div className="grid grid-flow-row h-[60%] border-r border-black">
+          {shop.map((item, i) => (
+            <Link
+            onMouseOver={() => {item === "Watches" && setWatches(true)}}
+              className="pl-6 hover:bg-gray-200 flex items-center"
+              to={item}
+            >
+              {" "}
+              {item}
+              <RiArrowRightSLine className="mt-1" />
+            </Link>
+          ))}
+        </div>
+        {/* //////////////////////////////////// */}
+        {watches && <div className="grid grid-flow-row h-[40%] border-r border-black">
+          {watchShop.map((item) => (
+            <Link
+              className="pl-6 hover:bg-gray-200 flex items-center"
+              to={item}
+            >
+              {" "}
+              {item}
+              <RiArrowRightSLine className="mt-1" />
+            </Link>
+          ))}
+        </div>}
+        {/* //////////////////////////////////// */}
+        {brands && <div className="grid grid-flow-row h-[100%] border-r border-black">
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link1}
           >
-            <div className="text-xl font-semibold pl-6 flex items-center">
-              {item.type}
-            </div>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center"
-              to={item.link1}
-            >
-              {" "}
-              {item.f1}
-            </Link>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center"
-              to={item.link2}
-            >
-              {" "}
-              {item.f2}
-            </Link>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center"
-              to={item.link3}
-            >
-              {" "}
-              {item.f3}
-            </Link>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center"
-              to={item.link4}
-            >
-              {item.f4}
-            </Link>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center"
-              to={item.link5}
-            >
-              {item.f5}
-            </Link>
-            <Link
-              className="pl-6 hover:bg-gray-200 flex items-center text-yellow-600 text-[14px]"
-              to={item.link6}
-            >
-              {item.f6}
-              <span className="mt-[5px]">
-                <RiArrowRightSLine />
-              </span>
-            </Link>
-          </div>
-        ))}
+            {" "}
+            {watchBrands.f1}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link2}
+          >
+            {" "}
+            {watchBrands.f2}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link3}
+          >
+            {" "}
+            {watchBrands.f3}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link4}
+          >
+            {" "}
+            {watchBrands.f4}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link5}
+          >
+            {" "}
+            {watchBrands.f5}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchBrands.link6}
+          >
+            {" "}
+            {watchBrands.f6}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+        </div>}
+        {/* //////////////////////////////////// */}
+        {collections && <div className="grid grid-flow-row h-[100%] border-r border-black">
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link1}
+          >
+            {" "}
+            {watchCollections.f1}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link2}
+          >
+            {" "}
+            {watchCollections.f2}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link3}
+          >
+            {" "}
+            {watchCollections.f3}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link4}
+          >
+            {" "}
+            {watchCollections.f4}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link5}
+          >
+            {" "}
+            {watchCollections.f5}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+          <Link
+            className="pl-6 hover:bg-gray-200 flex items-center"
+            to={watchCollections.link6}
+          >
+            {" "}
+            {watchCollections.f6}
+            <RiArrowRightSLine className="mt-1" />
+          </Link>
+        </div>}
       </div>
     </div>
   );
